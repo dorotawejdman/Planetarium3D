a6f6d5645dea496c43898479ba24ecb631439542
dorotawejdman <dorota.wejdman@gmail.com>
2022-03-27 22:13:10 +0200

    Create node server and first opengl object

--- public/Test1.js
+++ public/Test1.js
@@ -18,7 +18,7 @@
 // };
 
 const myHeaders = new Headers();
-// myHeaders.append("Access-Control-Allow-Origin", "https://ssd.jpl.nasa.gov");
+// myHeaders.append("Access-Control-Allow-Origin", "*");
 const myRequest = new Request(
   "https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='499'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@399'&START_TIME='2006-01-01'&STOP_TIME='2006-01-20'&STEP_SIZE='1%20d'&QUANTITIES='1,9,20,23,24,29'",
   {
