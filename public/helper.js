3d251c6ed9de2b2d2a63ed34cf97570559b5b357
dorotawejdman <dorota.wejdman@gmail.com>
2022-04-03 20:02:00 +0200

    Create spinning triangle

---
+++ public/helper.js
@@ -0,0 +1,6 @@
+function logMatrix(mat) {
+  console.log(mat[0], mat[1], mat[2], mat[3]);
+  console.log(mat[4], mat[5], mat[6], mat[7]);
+  console.log(mat[8], mat[9], mat[10], mat[11]);
+  console.log(mat[12], mat[13], mat[14], mat[15]);
+}
